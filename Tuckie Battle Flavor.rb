04d876f1b095a3fecc706fa58d2e040a80cebb04
Tuckie <votetuckie@users.noreply.github.com>
2016-12-03 00:36:46 -0800

    optimization, strings now from alive enemies
    bug: on default battle results, no call is made to delete @window once other elements are deleted

--- Tuckie Battle Flavor.rb
+++ Tuckie Battle Flavor.rb
@@ -1,122 +1,63 @@
-=begin
-
-  Flavor Text in Battles
-  Made By Tuckie (tucker.johnson.qcd@gmail.com)
-  Date : 11 June 2016
-
-  Version: Not Done
-
-(imagine "-" hyphens)
-
-
-
-  This script does cool stuff that makes games better.
-
-________________________________________________________________________________
- Instructions ======================================================  _ [ ] [X]
-|______________________________________________________________________________|
-
-  Put this script somewhere between the "materials" item and the "Main" script.
-  Mind the pit of snakes on B2F.
-
-|______________________________________________________________________________|
-
-________________________________________________________________________________
- Terms Of Use ======================================================  _ [ ] [X]
-|______________________________________________________________________________|
-
-  Feel free to use this script in commercial or free/donation based games.
-  I luv u
-  If you are making a for-profit game, though, it wouldn't hurt to send a key
-  my way. ;()
-
-|______________________________________________________________________________|
-
-=end
-
 module Tuckie_BattleFlavor
   # X and Y locations of displayed text, where (0,0) is the top right
   FLTEXT_X =  0
-  FLTEXT_Y =  0
+  FLTEXT_Y =  -5
 
   #--------------------------------------------------------------------------
   # TEXT CUSTOMIZATION
   #   Each Entry into the array below represents the set of quotes for one
   #   troop.
   #   The first Entry are phrases that will display if you have not
-  #   created an array for a troop yet. (i.e if the troop id is higher than
+  #   created an array for an enemy yet. (i.e if the enemy id is higher than
   #   the number of arrays you have set.
   #--------------------------------------------------------------------------
 
   ENEMY_FLAVOR =[
 
     ["It's Time To Slam Jam", "Watch Out For My Secret Move!"], # Default Phrases. Array index 0
-    [nil],#troop 1
-    [nil],#troop 2
-    [],#troop 3
-    ["It smells like robots.",
-    "Tuckie is being ironic, and stuff.", "We're gonna need to replace the carpet."]#troop 4
-
-
-
+    [nil],#boy 1
+    ["Someone's gotta clean up those ballots", "Chads. So many chads."], #ballot box
+    ["(-_-;)"], #wobbly nelson
+    []#boy 4
 
   ]# End of flavors
 
 end #Keep this here!! ;)
-
-class Tuckie_Flavortext_get < Game_Troop
-
-  def gettroopid
-    @@FlavorTroop = $game_troop.troop.id  #.alive_members[@index]
-    getflavstrings(@@FlavorTroop)
-  end
-
-  def getflavstrings(troop)
-    if Tuckie_BattleFlavor::ENEMY_FLAVOR[troop] != nil
-      @@FlavStrings = Tuckie_BattleFlavor::ENEMY_FLAVOR[troop]
-      flavlength = @@FlavStrings.count
-      #flavlength -= 1
-      flavor = @@FlavStrings[rand(0 - flavlength)]
-      return flavor
-    else
-      @@FlavStrings = Tuckie_BattleFlavor::ENEMY_FLAVOR[0]
-      flavlength = @@FlavStrings.count
-      #flavlength -= 1
-      flavor = @@FlavStrings[rand(0 - flavlength)]
-      return flavor
-    end
-  end
-
-end
-
-
-
-
 
   #--------------------------------------------------------------------------
   # * Window that displays flavor text
   #--------------------------------------------------------------------------
 class BattleFlavorWindow < Window_Base
 
-    def initialize
-      super(-6, -12, 648, 480)
-      self.z = 299
-      self.back_opacity = 0
+  def initialize
+    super(-6, -12, 648, 480)
+    self.z = 299
+    self.back_opacity = 0
+  end
+    
+  def gettroopid
+    flavortroop = $game_troop.alive_members.sample.enemy_id
+    if Tuckie_BattleFlavor::ENEMY_FLAVOR[flavortroop] != nil
+      return Tuckie_BattleFlavor::ENEMY_FLAVOR[flavortroop].sample
+    else
+      return Tuckie_BattleFlavor::ENEMY_FLAVOR[0].sample
     end
+  end
 
-    def change_text (string = "No Text Detected")
-      return unless string.is_a?(String)
-      contents.clear
-      draw_text( Tuckie_BattleFlavor::FLTEXT_X, Tuckie_BattleFlavor::FLTEXT_Y,
-      640, 32, string, 0)
-    end
+  def change_text (string = "No Text Detected")
+    return unless string.is_a?(String)
+    contents.clear
+    draw_text( Tuckie_BattleFlavor::FLTEXT_X, Tuckie_BattleFlavor::FLTEXT_Y,
+    640, 32, string, 0)
+  end
 
-    def draw_text(x, y, text_width, text_height, string, allignment = 0)
-      contents.draw_text(x, y, text_width, text_height, string, allignment)
-    end
-    def destroy
-      contents.clear
-    end
+  def draw_text(x, y, text_width, text_height, string, allignment = 0)
+    contents.draw_text(x, y, text_width, text_height, string, allignment)
+  end
+  
+  def destroy
+    contents.clear
+  end
 end #class BattleFlavorWindow
 
   #--------------------------------------------------------------------------
@@ -124,31 +65,34 @@ end #class BattleFlavorWindow
   #--------------------------------------------------------------------------
 class Scene_Battle < Scene_Base
 
-  def initialize()
-  super
-    @@window = BattleFlavorWindow.new
+  alias tuckie_flavor_init initialize
+  def initialize(*args)
+    tuckie_flavor_init(*args)
+    @window = BattleFlavorWindow.new
   end # initialize
 
-    alias Tuckie_DrawFlavorWindow   start_party_command_selection
-    def start_party_command_selection
-      Tuckie_DrawFlavorWindow()
-      @@window = BattleFlavorWindow.new
-      tftget = Tuckie_Flavortext_get.new
-      @@window.change_text(tftget.gettroopid)
+  alias tuckie_drawflavorwindow   start_party_command_selection
+  def start_party_command_selection
+    tuckie_drawflavorwindow()
+    @window.change_text(@window.gettroopid)
+  end # create_all_windows
 
-    end # create_all_windows
+  alias tuckie_turn_start turn_start
+  def turn_start
+    tuckie_turn_start()
+    @window.destroy() if @window != nil
+  end
 
-    alias Tuckie_DelFlavorWindow turn_start
-    def turn_start
-      Tuckie_DelFlavorWindow()
-        if @@window != nil
-        @@window.dispose
-        end
-    end
+  alias tuckie_flavorescape command_escape
+  def command_escape
+    @window.dispose
+    tuckie_flavorescape()
+  end
 
-    alias Tuckie_FlavorEscape command_escape
-    def command_escape
-        @@window.dispose
-      Tuckie_FlavorEscape()
-    end
+  alias tuckie_flavor_terminate terminate  
+  def terminate
+    @window.dispose
+    tuckie_flavor_terminate()
+  end
+  
 end# class BattleFlavor
