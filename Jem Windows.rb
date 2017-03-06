b81c1416b0504b714940b229b31955fcc0b7a30a
Tuckie <thewafflier@gmail.com>
2017-03-06 09:42:13 -0800

    :gem: working :()

--- Jem Windows.rb
+++ Jem Windows.rb
@@ -9,26 +9,19 @@
 #==============================================================================
 
   Sprites = [
-    [Cache.system("Jem_normal.png"), Cache.system("Jem_normal_1.png")], # 0
-    [Cache.system("Jem_kiss.png"), Cache.system("Jem_kiss_1.png")]
+    ["Graphics/System/Jem_normal.png", "Graphics/System/Jem_normal_1.png"], # 0
+    #[Cache.system("Jem_kiss.png"), Cache.system("Jem_kiss_1.png")]
   ]
 
-  CONFIG = { # 👏 do 👏 not 👏 delete 👏 this 👏 pls 👏
-
-    img_src     => Sprites[0] # 📷
-    speed       => 0.1
-
+    SPEED       = 0.1
     #---------------------------------------------------------------------------
-    shake       => false
-    shake_pow   => 4
-    shake_len   => 8
+    SHAKE       = false
+    SHAKE_POW   = 4
+    SHAKE_LEN   = 8
     #---------------------------------------------------------------------------
-    angle_max   => 20
-
-    height_max  => 16
-
-    width_max   => 4
-  } # keep this curly boy, keep him safe forever :)
+    ANGLE_MAX   = 4
+    HEIGHT_MAX  = 12
+    WIDTH_MAX   = -12
 
 end
 
@@ -39,32 +32,35 @@ end
 #==============================================================================
 
 class Sprite_jem < Sprite # 🔥 💎 🔥 it yo boy
-  attr_accessor :attributes
-  attr_accessor :jem
+    attr_accessor :img_src
+    attr_accessor :speed
+    attr_accessor :shake
+    attr_accessor :shake_pow
+    attr_accessor :shake_len
+    #---------------------------------------------------------------------------
+    attr_accessor :angle_max
+    attr_accessor :height_max
+    attr_accessor :width_max
 
-  def initialize(attrs)
-    @attributes = attrs
+  def initialize(*args)
     @count = 0
+    set_offset()
+    super(*args)
   end
 
-  def jem_bitmap_update(force)
-
-    if force
-      self.bitmap = Bitmap.new(@attributes[img_src.sample])
-    else
-      if @count % 16 == 0 and @count != 0
-        self.bitmap = Bitmap.new(@attributes[img_src.sample])
-      end
-
-    end
-
+  def set_offset()
+    @off_x = 80
   end
 
-  def update()
-    jem_bitmap_update(false)
-    self.oy = @attributes[height_max] * Math.sin( @count * @attributes[speed] )
-    self.ox = @attributes[width_max]  * Math.sin( @count * @attributes[speed] )
+  def update(*args)
+    self.bitmap = Bitmap.new(@img_src.sample) if @count % 8 == 0
+    self.oy     = @height_max * Math.sin(@count * 0.1)
+    self.ox     = @width_max  * Math.cos(@count * 0.025)
+    self.angle  = @angle_max  * Math.cos(@count * 0.1)
+    self.ox += @off_x
+    @off_x = @off_x * 0.94
     @count += 1
+    super(*args)
   end
 
 end
@@ -76,8 +72,15 @@ end
 #==============================================================================
 
 class Window_Message < Window_Base
-  attr_accessor :attributes
-  attr_accessor :jem
+    attr_accessor :img_src
+    attr_accessor :speed
+    attr_accessor :shake
+    attr_accessor :shake_pow
+    attr_accessor :shake_len
+    #---------------------------------------------------------------------------
+    attr_accessor :angle_max
+    attr_accessor :height_max
+    attr_accessor :width_max
 
   #--------------------------------------------------------------------------
   # * Window_Message Creation | Alias
@@ -85,17 +88,43 @@ class Window_Message < Window_Base
   alias tuckie_jem_initialize initialize
   def initialize
     tuckie_jem_initialize
-    @attributes = WindowJem::CONFIG # sets to user setting default
-    @jem = []
+    get_attributes()
+  end
+
+  #--------------------------------------------------------------------------
+  # * get attributes | New Method
+  #--------------------------------------------------------------------------
+  def get_attributes
+   @img_src    = WindowJem::Sprites[0]
+   @speed      = WindowJem::SPEED
+   @shake      = WindowJem::SHAKE
+   @shake_pow  = WindowJem::SHAKE_POW
+   @shake_len  = WindowJem::SHAKE_LEN
+   @angle_max  = WindowJem::ANGLE_MAX
+   @height_max = WindowJem::HEIGHT_MAX
+   @width_max  = WindowJem::WIDTH_MAX
   end
 
   #--------------------------------------------------------------------------
   # * Jem sprite creation | New Method
   #--------------------------------------------------------------------------
   def create_jem()
-    jem = Sprite.new()
+    @jem = Sprite_jem.new()
+    #bitmap          = Bitmap.new("Graphics/System/Jem_normal.png")
+    #@jem.bitmap     = bitmap
+    @jem.img_src    = @img_src
+    @jem.speed      = @speed
+    @jem.shake      = @shake
+    @jem.shake_pow  = @shake_pow
+    @jem.shake_len  = @shake_len
+    @jem.angle_max  = @angle_max
+    @jem.height_max = @height_max
+    @jem.width_max  = @width_max
 
-    @jem.push(jem)
+    @jem.x =  380
+    @jem.y =  20
+    @jem.z = 500
+    @jem.visible = true
   end
 
   #--------------------------------------------------------------------------
@@ -103,40 +132,38 @@ class Window_Message < Window_Base
   #     code : the core of the control character
   #            e.g. "C" in the case of the control character \C[1].
   #--------------------------------------------------------------------------
-  alias tuckie_wiggly_process_escape_character process_escape_character
+  alias tuckie_jem_process_escape_character process_escape_character
   def process_escape_character(code, text, pos)
     case code.upcase
     when 'HAT' # here comes our hat boy, watch out everyone | starts JEM
       create_jem()
     when 'JEM'
-      @attributes[img_src] = WindowJem::Sprites[obtain_escape_param(text)]
-    when 'JEMSHK' # uh oh bye bye jem see u next time
+      @img_src = WindowJem::Sprites[obtain_escape_param(text)]
+    when 'JEMSHK'
 
-    when 'JEMOFF'
-
+    when 'JEMOFF' # uh oh bye bye jem see u next time
+      jem_dispose()
     end
-    tuckie_wiggly_process_escape_character(code, text, pos)
+    tuckie_jem_process_escape_character(code, text, pos)
   end
 
   #--------------------------------------------------------------------------
   # * Close Window and Wait for It to Fully Close | Alias
   #--------------------------------------------------------------------------
   def jem_dispose()
-    unless @jem.empty?
-      @jem.each do |hat|
-        hat.dispose()
-      end
-    end
-    @attributes = WindowJem::CONFIG
+    @jem.dispose() if defined? @jem != nil
+    @jem = Sprite_jem.new
+    @jem.visible = false
+    get_attributes()
   end
 
   #--------------------------------------------------------------------------
   # * Close Window and Wait for It to Fully Close | Alias
   #--------------------------------------------------------------------------
-  alias tuckie_wiggly_close_wait close_and_wait
+  alias tuckie_jem_close_wait close_and_wait
   def close_and_wait
     jem_dispose()
-    tuckie_wiggly_close_wait()
+    tuckie_jem_close_wait()
   end
 
   #--------------------------------------------------------------------------
@@ -151,10 +178,10 @@ class Window_Message < Window_Base
   #--------------------------------------------------------------------------
   # * New Page | Alias
   #--------------------------------------------------------------------------
-  alias tuckie_wiggly_new_page new_page
+  alias tuckie_jem_new_page new_page
   def new_page(*args)
-    #idk meybe this could be useful lol prolly not
-    tuckie_wiggly_new_page(*args)
+    @img_src = WindowJem::Sprites[0]
+    tuckie_jem_new_page(*args)
   end
 
   #--------------------------------------------------------------------------
@@ -170,10 +197,18 @@ class Window_Message < Window_Base
   # * Text Animation Processing | New Method
   #--------------------------------------------------------------------------
   def update_jem()
-    unless @jem.empty?
-      @jem.each do |hat|
-        hat.update()
-      end
+    if defined? @jem != nil
+
+      @jem.img_src    = @img_src
+      @jem.speed      = @speed
+      @jem.shake      = @shake
+      @jem.shake_pow  = @shake_pow
+      @jem.shake_len  = @shake_len
+      @jem.angle_max  = @angle_max
+      @jem.height_max = @height_max
+      @jem.width_max  = @width_max
+
+      @jem.update
     end
   end
 
